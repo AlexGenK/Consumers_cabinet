b65278bb943dad545ad47603f86fe41ba8f5029a
Oleksii Kozhyn <suburbakskin@gmail.com>
2018-10-18 22:32:02 +0300

    Tested Consumer model

--- spec/factories/consumers_factory.rb
+++ spec/factories/consumers_factory.rb
@@ -1,9 +1,14 @@
 FactoryBot.define do
   factory :consumer, class: Consumer do
-    name        { 'ЧП Пупкин В.С.' }
-    onec_id     { 123 }
-    edrpou      { '01234567' }
-    dog_eh_num  { '1/2' }
-    dog_eh_date { '12/05/2005' }
+    name          { 'ЧП Пупкин В.С.' }
+    onec_id       { 123 }
+    edrpou        { '01234567' }
+    dog_eh_num    { '1/2' }
+    dog_eh_date   { '12/05/2005' }
+    dog_hoe_num   { '10/12' }
+    dog_hoe_date  { '24/01/2005' }
+    report_date   { 25 }
+    user_email    { 'user@mail.no' }
+    manager_email { 'manager@mail.no' }
   end
 end
--- spec/models/consumer_spec.rb
+++ spec/models/consumer_spec.rb
@@ -1,5 +1,29 @@
 require 'rails_helper'
 
 RSpec.describe Consumer, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  subject { build(:consumer) }
+
+  it "is valid with valid attributes" do
+    expect(subject).to be_valid
+  end
+
+  it "is not valid with empty name" do
+    subject.name = ''
+    expect(subject).to_not be_valid
+  end
+
+  it "is not valid with empty 1C Id" do
+    subject.onec_id = ''
+    expect(subject).to_not be_valid
+  end
+
+  it "is not valid when name is not unique" do
+    create(:consumer, onec_id: 124)
+    expect(subject).to_not be_valid
+  end
+
+  it "is not valid when 1C Id is not unique" do
+    create(:consumer, name: 'ЧП Попкин С.В.')
+    expect(subject).to_not be_valid
+  end
 end
