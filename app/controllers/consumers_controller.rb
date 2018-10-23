948a0a9d7d03460b78d09c9d30d0cc4eae4e7c00
Oleksii Kozhyn <suburbakskin@gmail.com>
2018-10-23 17:08:12 +0300

    Realised index, new, create and destroy actions to the Consumers controller

--- app/controllers/consumers_controller.rb
+++ app/controllers/consumers_controller.rb
@@ -5,14 +5,45 @@ class ConsumersController < ApplicationController
     @consumers=Consumer.order(:name)
   end
 
+  def new
+    @consumer = Consumer.new
+  end
+
+  def create
+    @consumer = Consumer.new(consumer_params)
+    if @consumer.save
+      redirect_to consumers_path
+    else
+      flash[:alert] = 'Невозможно создать потребителя'
+      render :new
+    end
+  end
+
+  def destroy
+    Consumer.find(params[:id]).destroy
+    redirect_to consumers_path
+  end
+
+  def edit
+  end
+
+  def update
+    if @consumer.update(cоnsumer_params)
+        redirect_to consumers_path, notice: 'Потребитель успешно отредактирован'
+      else
+        flash[:alert] = 'Невозможно создать потребителя'
+        render :edit
+      end
+  end
+
   private
 
   def set_consumer
-    @author = Consumer.find(params[:id])
+    @consumer = Consumer.find(params[:id])
   end
 
   def consumer_params
-    params.require(:author).permit(:name, :edrpou, :dog_eh_num, :dog_eh_date, :dog_hoe_num, :dog_hoe_date, 
+    params.require(:consumer).permit(:name, :edrpou, :dog_eh_num, :dog_eh_date, :dog_hoe_num, :dog_hoe_date, 
                                     :onec_id, :report_date, :user_email, :manager_email)
   end
 
