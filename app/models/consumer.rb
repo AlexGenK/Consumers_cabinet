168332fd82e4e5be1dd35f95614a49b67d882ccd
Oleksii Kozhyn <suburbakskin@gmail.com>
2018-10-24 21:50:49 +0300

    Fix has_many relation in the Consumer and Counter models

--- app/models/consumer.rb
+++ app/models/consumer.rb
@@ -1,6 +1,6 @@
 class Consumer < ApplicationRecord
+  has_many :counters
+
   validates :name, :onec_id, presence: true
   validates :name, :onec_id, uniqueness: true
-
-  has_many :cоunters
 end
--- app/models/counter.rb
+++ app/models/counter.rb
@@ -1,6 +1,7 @@
 class Counter < ApplicationRecord
   has_many :powers
   belongs_to :consumer
+
   validates :number, presence: true
   validates :ratio, numericality: { greater_than: 0 }
 end
