667c1542900bd8b507aff3d54a6bf9498f0ca0d5
Oleksii Kozhyn <suburbakskin@gmail.com>
2018-11-02 20:53:46 +0200

    Import consumers from file

--- app/controllers/admin/filling_consumers_controller.rb
+++ app/controllers/admin/filling_consumers_controller.rb
@@ -5,9 +5,24 @@ class Admin::FillingConsumersController < ApplicationController
   end
 
   def start
-    csv_text = params[:datafile].read.encode('UTF-8', 'Windows-1251')
-    csv_text.gsub!(/"/,'\'')
-    @csv = CSV.parse(csv_text, col_sep: ';')
+    begin
+      csv_text = params[:datafile].read.encode('UTF-8', 'Windows-1251').gsub(/"/,'\'')
+      csv = CSV.parse(csv_text, col_sep: ';')
+      @imported = []
+      csv.each do |record|
+        unless Consumer.exists?(onec_id: record[0].to_i)
+          Consumer.new(name: record[1],
+                      edrpou: record[2],
+                      dog_eh_num: record[3],
+                      dog_eh_date: Date.strptime(record[4], '%d.%m.%y'),
+                      onec_id: record[0].to_i).save
+          @imported << record
+        end
+      end
+    rescue
+      flash[:alert] = 'Ошибка при импорте записей из файла!'
+      @imported = []
+    end
   end
 
   private
