7d66d698f4e52142e984881f352e49257522d75f
Oleksii Kozhyn <suburbakskin@gmail.com>
2018-11-03 10:53:27 +0200

    Implemented import of previous consumption

--- app/controllers/admin/filling_consumptions_controller.rb
+++ app/controllers/admin/filling_consumptions_controller.rb
@@ -6,22 +6,23 @@ class Admin::FillingConsumptionsController < ApplicationController
 
   def start
     begin
-      # csv_text = params[:datafile].read.encode('UTF-8', 'Windows-1251').gsub(/"/,'\'')
-      # csv = CSV.parse(csv_text, col_sep: ';')
-      # @imported = []
-      # csv.each do |record|
-      #   unless Consumer.exists?(onec_id: record[0].to_i)
-      #     Consumer.new(name: record[1],
-      #                 edrpou: record[2],
-      #                 dog_eh_num: record[3],
-      #                 dog_eh_date: Date.strptime(record[4], '%d.%m.%y'),
-      #                 onec_id: record[0].to_i).save
-      #     @imported << record
-      #   end
-      # end
+      csv_text = params[:datafile].read.encode('UTF-8', 'Windows-1251').gsub(/"/,'\'')
+      csv = CSV.parse(csv_text, col_sep: ';')
+      @imported = 0
+      PreviousConsumption.delete_all
+      csv.each do |record|
+        @consumer = Consumer.find_by(onec_id: record[0].to_i)
+        if @consumer
+          @consumer.previous_consumptions.new(date: Date.strptime(record[1], '%d.%m.%y'),
+                                              power: record[2].to_i,
+                                              tariff: record[3].to_f,
+                                              money: record[4].to_f).save
+          @imported += 1
+        end
+      end
     rescue
       flash[:alert] = 'Ошибка при импорте записей из файла!'
-      @imported = []
+      @imported = 0
     end
   end
 
