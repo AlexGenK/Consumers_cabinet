337a3315cd40fb21ba4c99fe49dce89c92b64b8c
Oleksii Kozhyn <suburbakskin@gmail.com>
2018-11-06 22:44:25 +0200

    Implemented authentication with Devise gem

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_11_04_151925) do
+ActiveRecord::Schema.define(version: 2018_11_06_184038) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -73,6 +73,20 @@ ActiveRecord::Schema.define(version: 2018_11_04_151925) do
     t.datetime "updated_at", null: false
     t.bigint "consumer_id"
     t.index ["consumer_id"], name: "index_previous_consumptions_on_consumer_id"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "username", default: "", null: false
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+    t.index ["username"], name: "index_users_on_username", unique: true
   end
 
   add_foreign_key "previous_consumptions", "consumers"
