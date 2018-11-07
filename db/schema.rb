b299d5e276460c01fba137c19a441d0963358a51
Oleksii Kozhyn <suburbakskin@gmail.com>
2018-11-07 21:26:40 +0200

    Install CanCanCan gem

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_11_06_184038) do
+ActiveRecord::Schema.define(version: 2018_11_07_191651) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -84,6 +84,9 @@ ActiveRecord::Schema.define(version: 2018_11_06_184038) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.boolean "admin_role", default: false
+    t.boolean "manager_role", default: false
+    t.boolean "client_role", default: true
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
     t.index ["username"], name: "index_users_on_username", unique: true
