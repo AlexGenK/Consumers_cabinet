88e256377da56e224aca8971284856ebb8c8fce3
Oleksii Kozhyn <suburbakskin@gmail.com>
2018-11-08 23:39:21 +0200

    Redirect back to current page after sign up

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,9 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :null_session
 
   before_action :configure_permitted_parameters, if: :devise_controller?
+  before_action :store_user_location!, if: :storable_location?
   before_action :authenticate_user!
+
 
   protected
 
@@ -11,6 +13,14 @@ class ApplicationController < ActionController::Base
     redirect_back fallback_location: root_path
   end
 
+  def storable_location?
+    request.get? && is_navigational_format? && !devise_controller? && !request.xhr? 
+  end
+
+  def store_user_location!
+    store_location_for(:user, request.fullpath)
+  end
+  
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:username, :email])
   end
