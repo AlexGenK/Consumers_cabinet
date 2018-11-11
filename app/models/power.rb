a181f64e49a4375b30734ac32b78d63cd1d186b7
Oleksii Kozhyn <suburbakskin@gmail.com>
2018-11-11 11:29:54 +0200

    Implemented result methods for Power model

--- app/models/power.rb
+++ app/models/power.rb
@@ -1,3 +1,16 @@
 class Power < ApplicationRecord
   belongs_to :counter
+
+  def active_result
+    active - before_active
+  end
+
+  def reactive_result
+    reactive - before_reactive
+  end
+
+  def generation_result
+    generation - before_generation
+  end
+
 end
