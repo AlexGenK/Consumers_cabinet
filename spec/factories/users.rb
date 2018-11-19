79f9818970efea489c8f562b84828b9eee62a76e
Oleksii Kozhyn <suburbakskin@gmail.com>
2018-11-19 22:50:16 +0200

    Added more spec for the User model

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,5 +1,28 @@
 FactoryBot.define do
-  factory :user do
-    
+  factory :user_admin, class: User do
+    username      { 'admin' }
+    email         { 'admin@mail.no' }
+    password      { '123456' }
+    admin_role    { true }
+    manager_role  { false }
+    client_role   { false }
+  end
+
+  factory :user_manager, class: User do
+    username      { 'manager' }
+    email         { 'manager@mail.no' }
+    password      { '123456' }
+    admin_role    { false }
+    manager_role  { true }
+    client_role   { false }
+  end
+
+  factory :user_client, class: User do
+    username      { 'client' }
+    email         { 'client@mail.no' }
+    password      { '123456' }
+    admin_role    { false }
+    manager_role  { false }
+    client_role   { true }
   end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,19 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  subject { build(:user_admin) }
+
+  it "is valid with valid attributes" do
+    expect(subject).to be_valid
+  end
+
+  it "is not valid with a non-unique username" do
+    create(:user_admin, email: ('other' + subject.email))
+    expect(subject).to_not be_valid
+  end
+
+  it "is not valid with a non-unique email" do
+    create(:user_admin, username: ('other' + subject.username))
+    expect(subject).to_not be_valid
+  end
 end
