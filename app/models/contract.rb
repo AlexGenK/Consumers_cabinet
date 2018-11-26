57c06a7d98eb053ed322df78a7a17dfa9cffb0aa
Oleksii Kozhyn <suburbakskin@gmail.com>
2018-11-27 01:33:54 +0200

    Added full info of powers to the pdf report

--- app/models/contract.rb
+++ app/models/contract.rb
@@ -9,21 +9,42 @@ class Contract < ApplicationRecord
       pdf.move_down 20
       pdf.text "Отчет за #{I18n.l(rep_date, format: '%B %Y')} по дог. №#{number}"
       pdf.move_down 20
+      pdf.font_size 8
       @counters = counters.all.order(:number)
+      @all_power_active, @all_power_reactive, @all_power_generation = 0, 0, 0
       @table = []
+      @table << ['', '', 'Наст.', 'Пред.', 'Разность', 'Расход']
       @counters.each do |item|
         power = item.powers.where("measure_date >= ? AND measure_date <= ?",
                                 rep_date.beginning_of_month.to_date,
                                 rep_date.end_of_month.to_date).last
         if power
-          @table << ['', '', 'Наст.', 'Пред.', 'Разность', 'Расход']
-          @table << ["Счетчик №#{item.number}", 'Активная', "#{power.active}", "#{power.before_active}", '', '']
-          @table << ['', 'Реактивная', "#{power.reactive}", "#{power.before_reactive}", '', '']
-          @table << ['', 'Генерация', "#{power.generation}", "#{power.before_generation}", '', '']
-          @table << ['', '', '', '', '', '']
+          @table << ["Счетчик №#{item.number}, КУ=#{item.ratio}", 'Активная', "#{power.active}", "#{power.before_active}", "#{power.active_result}", "#{power.active_result * item.ratio}"]
+          @table << ['', 'Реактивная', "#{power.reactive}", "#{power.before_reactive}", "#{power.reactive_result}", "#{power.reactive_result * item.ratio}"]
+          @table << ['', 'Генерация', "#{power.generation}", "#{power.before_generation}", "#{power.generation_result}", "#{power.generation_result * item.ratio}"]
+
+          @all_power_active += power.active_result * item.ratio
+          @all_power_reactive += power.reactive_result * item.ratio
+          @all_power_generation += power.generation_result * item.ratio
         end
       end
-      pdf.table(@table)
+      pdf.table(@table) do |t|
+          t.cells.border_width = 1
+      end
+      pdf.move_down 20
+
+      @table = []
+      @table << [{content: "Всего", colspan: 2}]
+      @table << ['Активная', "#{@all_power_active}"]
+      @table << ['Реактивная', "#{@all_power_reactive}"]
+      @table << ['Генерация', "#{@all_power_generation}"]
+      pdf.table(@table) do |t|
+          t.cells.border_width = 1
+      end
+
+      pdf.move_down 40
+      pdf.font_size 7
+      pdf.text DateTime.now.strftime('Сгенерировано %F в %T')
       return pdf
   end
 end
