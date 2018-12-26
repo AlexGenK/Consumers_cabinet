b9b0a29855755a258b8edf7890e7e59e497cc0c0
Oleksii Kozhyn <suburbakskin@gmail.com>
2018-12-26 16:07:06 +0200

    Fix Counter#all_current_power error when the power value is not exist

--- app/models/counter.rb
+++ app/models/counter.rb
@@ -9,8 +9,12 @@ class Counter < ApplicationRecord
     power = powers.where("measure_date >= ? AND measure_date <= ?",
                               DateTime.now.beginning_of_month.to_date,
                               DateTime.now.end_of_month.to_date).last
-    ((power.active_result * ratio) +
-    (power.reactive_result * ratio) +
-    (power.generation_result * ratio)).round
+    if power
+      return ((power.active_result * ratio) +
+              (power.reactive_result * ratio) +
+              (power.generation_result * ratio)).round
+    else
+      return 0
+    end
   end
 end
