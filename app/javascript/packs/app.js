8152899a566f7a9f7b51582c0e0d7efc0ecd3a94
Oleksii Kozhyn <suburbakskin@gmail.com>
2019-05-26 19:42:25 +0300

    Replaced checkbox in the counter form to toggler

---
+++ app/javascript/packs/app.js
@@ -0,0 +1,7 @@
+console.log('Hello World from Webpacker');
+import 'bootstrap4-toggle/js/bootstrap4-toggle';
+import 'bootstrap4-toggle/css/bootstrap4-toggle.css';
+
+$(document).on('ready turbolinks:load', function (e) {
+   $('input[type=checkbox][data-toggle^=toggle]').bootstrapToggle()
+});
