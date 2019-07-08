e4647c1d4349317a149226e6c45ad03c94fb62f6
Oleksii Kozhhyn <suburbakskin@gmail.com>
2019-07-08 21:04:41 +0300

    Added homepage spec for login process

--- spec/features/homepage_spec.rb
+++ spec/features/homepage_spec.rb
@@ -11,10 +11,24 @@ feature 'View homepage and access to the consumers informations', type: :feature
     expect(page).to have_content 'Пароль'
   end
 
-  scenario 'Visitor can log in' do
+  scenario 'Visitor can log in with right login/password' do
     fill_in 'Имя пользователя', with: @user.username
     fill_in 'Пароль', with: @user.password
     click_button 'Войти'
     expect(page).to have_content 'Вход в систему выполнен'
+  end
+
+  scenario 'Visitor can not log in with incorrect login' do
+    fill_in 'Имя пользователя', with: "#{@user.username}-incorrect"
+    fill_in 'Пароль', with: @user.password
+    click_button 'Войти'
+    expect(page).to have_content 'Неверное имя пользователя или пароль'
+  end
+
+  scenario 'Visitor can not log in with incorrect password' do
+    fill_in 'Имя пользователя', with: @user.username
+    fill_in 'Пароль', with: "#{@user.password}-incorrect"
+    click_button 'Войти'
+    expect(page).to have_content 'Неверное имя пользователя или пароль'
   end
 end
