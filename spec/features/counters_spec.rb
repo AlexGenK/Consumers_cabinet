6a34237ae4b75d3c71be6b353b2311de8c64c33c
Oleksii Kozhyn <suburbakskin@gmail.com>
2019-08-01 15:45:52 +0300

    Create skeleton of spec for counters features

---
+++ spec/features/counters_spec.rb
@@ -0,0 +1,104 @@
+require 'rails_helper'
+
+feature 'Access to contract counters', type: :feature do
+
+	before do
+		@admin = create(:user_admin)
+		@consumers = []
+
+		MANAGER_CLIENT_NAME.each do |man_name, cli_array|
+			@manager = create(:user_manager, username: man_name)
+			cli_array.each do |cli_name|
+				@client = create(:user_client, username: cli_name)
+				@consumers << create(:consumer, client_username: cli_name, manager_username: man_name)
+			end
+		end
+	end
+
+	context 'When Visitor is logged in as Admin' do
+
+		let(:selected_consumer) { @consumers[0] }
+		let(:selected_contract) { selected_consumer.contracts[0] }
+		let(:selected_counter)  { selected_contract.counters[0] }
+
+		before do
+			login_as @admin, :scope => :user
+			visit consumer_path(selected_consumer)
+			click_link selected_contract.number
+		end
+
+		scenario 'The Visitor can viewing index of counters', :focus do
+
+		end
+
+		scenario 'The Visitor can delete a counter' do
+
+		end
+
+		scenario 'The Visitor can edit a counter' do
+
+		end
+
+		scenario 'The Visitor can add a counter' do
+
+		end
+	end
+
+	context 'When Visitor is logged in as Manager' do
+
+		let(:selected_consumer) { @consumers.find { |cons| cons.manager_username == @manager.username } }
+		let(:selected_contract) { selected_consumer.contracts[0] }
+		let(:selected_counter)  { selected_contract.counters[0] }
+
+		before do 
+			login_as @manager, :scope => :user 
+			visit consumer_path(selected_consumer)
+			click_link selected_contract.number
+		end
+
+		scenario 'The Visitor can viewing index of counters' do
+
+		end
+
+		scenario 'The Visitor can delete a counter' do
+
+		end
+
+		scenario 'The Visitor can edit a counter' do
+
+		end
+
+		scenario 'The Visitor can add a counter' do
+
+		end
+	end
+
+	context 'When Visitor is logged in as Consumer' do
+
+		let(:selected_consumer) { @consumers.find { |cons| cons.client_username == @client.username } }
+		let(:selected_contract) { selected_consumer.contracts[0] }
+		let(:selected_counter)  { selected_contract.counters[0] }
+
+		before do 
+			login_as @client, :scope => :user 
+			visit consumer_path(selected_consumer)
+			click_link selected_contract.number
+		end
+
+		scenario 'The Visitor can viewing index of counters' do
+
+		end
+
+		scenario 'The Visitor can not delete a counter`' do
+
+		end
+
+		scenario 'The Visitor can not edit a counter' do
+
+		end
+
+		scenario 'The Visitor can not add a counter' do
+
+		end
+	end
+end
