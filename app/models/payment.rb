1b59fb019511d84f01fade6b6de2fd8339c7e051
Oleksii Kozhhyn <suburbakskin@gmail.com>
2019-09-21 15:11:39 +0300

    Change account validation to IBAN format

--- app/models/payment.rb
+++ app/models/payment.rb
@@ -1,7 +1,7 @@
 class Payment < ApplicationRecord
 	validates :company, presence: true
 	validates :edrpou, 	presence: true, length: { is: 8 }
-	validates :account, presence: true, numericality: { only_integer: true }
+	validates :account, presence: true, this_is_iban: true
 	validates :bank, 		presence: true
 	validates :mfo, 		presence: true, length: { is: 6 }
 end
