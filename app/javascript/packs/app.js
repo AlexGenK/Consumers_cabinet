6e6db9aa50d0ba9842ad3239045c4bd89807f642
Oleksii Kozhhyn <suburbakskin@gmail.com>
2019-09-22 17:09:08 +0300

    Connect minified version bootstrap4-toggle

--- app/javascript/packs/app.js
+++ app/javascript/packs/app.js
@@ -1,5 +1,5 @@
-import 'bootstrap4-toggle/js/bootstrap4-toggle';
-import 'bootstrap4-toggle/css/bootstrap4-toggle.css';
+import 'bootstrap4-toggle/js/bootstrap4-toggle.min.js';
+import 'bootstrap4-toggle/css/bootstrap4-toggle.min.css';
 
 $(document).on('ready turbolinks:load', function (e) {
    $('input[type=checkbox][data-toggle^=toggle]').bootstrapToggle()
