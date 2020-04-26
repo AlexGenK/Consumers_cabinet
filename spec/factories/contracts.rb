e8d78602ade0aea3194b034d63bac5a895349dd7
Oleksii Kozhhyn <suburbakskin@gmail.com>
2020-04-26 17:26:53 +0300

    Update the factories accord to new Faker syntax

--- spec/factories/contracts.rb
+++ spec/factories/contracts.rb
@@ -1,7 +1,7 @@
 FactoryBot.define do
   factory :contract do
     sequence(:number) { |n| "Договор-#{n}" }
-    date              { Faker::Date.between(5.years.ago, Date.today) }
+    date              { Faker::Date.between(from: 5.years.ago, to: Date.today) }
     association :consumer, factory: :consumer
 
     after :create do |contract|
--- spec/factories/payments.rb
+++ spec/factories/payments.rb
@@ -2,7 +2,7 @@ FactoryBot.define do
   factory :payment do
     company { Faker::Company.name }
     edrpou  { Faker::Company.edrpou }
-    account { Faker::Bank.iban('ua') }
+    account { Faker::Bank.iban(country_code: 'ua') }
     bank    { Faker::Bank.name }
     mfo     { Faker::Bank.mfo }
   end
