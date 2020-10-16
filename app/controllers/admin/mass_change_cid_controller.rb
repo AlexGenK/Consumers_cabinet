5832b239a0ca22732f08c86f3793a384ec2d7108
Oleksii Kozhyn <suburbakskin@gmail.com>
2020-10-16 15:54:39 +0300

    Implemented mass change 1C ID function to the controller

--- app/controllers/admin/mass_change_cid_controller.rb
+++ app/controllers/admin/mass_change_cid_controller.rb
@@ -8,6 +8,20 @@ class Admin::MassChangeCidController < ApplicationController
   end
 
   def start
+    begin
+      csv_text = params[:datafile].read.encode('UTF-8', 'Windows-1251')
+      csv = CSV.parse(csv_text)
+      @log = []
+      csv.each do |record|
+        @consumer = Consumer.find_by(onec_id: record[0].to_i)
+        @consumer.onec_id = record[1].to_i
+        @consumer.save!
+        @log << "#{@consumer.name} #{record[0]} => #{record[1]}"
+      end
+    rescue
+      flash[:alert] = 'Ошибка при импорте записей из файла!'
+      @imported = 0
+    end
   end
 
   private
